95da9bd6a0d866f11d472b8082e885e8ab0d647f
EstebanSansart <julianestebansanabria@gmail.com>
2023-07-18 07:11:25 -0500

    feat: :memo: fourthCommit

--- Infrastructure/Data/Migrations/20230714112651_InitialCreate.Designer.cs
+++ Infrastructure/Data/Migrations/20230714112651_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Data.Migrations
 {
     [DbContext(typeof(miEpsContext))]
-    partial class miEpsContextModelSnapshot : ModelSnapshot
+    [Migration("20230714112651_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
